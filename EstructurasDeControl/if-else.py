dbc9816be16d53d1d55d58b04eccbe7273b07ba5
Hugo Escalpelo <hugoescalpelo@gmail.com>
2022-10-07 10:08:11 -0500

    Update if-else.py

--- EstructurasDeControl/if-else.py
+++ EstructurasDeControl/if-else.py
@@ -1,7 +1,7 @@
 # Solicita un número del usuario 
 num = float(input("Ingresa un número: ")) 
 # incia estructura de control 
-if num == 0: 
+if num == 0:
     result = "El número es cero" 
 else: 
     result = "El número es diferente de cero" 
